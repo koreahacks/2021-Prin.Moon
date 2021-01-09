e6e36946b32d247319a8bf78e301f2f2745143b3
박진용 <jinyoung1997@gmail.com>
2021-01-09 23:49:20 +0900

    feat : pot api 들 구현
    pot detail 가져오기
    user가 join한 pot list 가져오기
    user가 생성한 팟리스트 가져오기 등 구현

--- server/src/pot/pot.service.ts
+++ server/src/pot/pot.service.ts
@@ -2,11 +2,38 @@ import { getRepository } from "typeorm";
 import { resMessage, statusCode } from "../common/constant";
 import JsonResponse from "../common/types/json-response";
 import PotEntity from "../entity/pot.entity";
+import UserJoinPotEntity from "../entity/user-join-pot.entity";
 import sortPotsByLocation from "../lib/sort-pot-by-location";
 import PotDTO from "./dto/pot.dto";
 import UpdatePotRequest from "./dto/update-pot-request.dto";
 
 const PotService = {
+  getUserJoinedPot: async (userId: number) => {
+    const userJoinPotReository = getRepository(UserJoinPotEntity);
+    const potList = await userJoinPotReository.find({
+      where: { userId },
+    });
+
+    return potList;
+  },
+
+  getUserOwnedPot: async (userId: number) => {
+    const potRepository = getRepository(PotEntity);
+    const potList = await potRepository.find({ where: { ownerId: userId } });
+
+    return potList;
+  },
+
+  getPotById: async (potId: number) => {
+    const potRepository = getRepository(PotEntity);
+    const potInfo = await potRepository.findOne({
+      relations: ["owner", "userJoinPot"],
+      where: { id: potId },
+    });
+
+    return potInfo;
+  },
+
   getRecentPots: async () => {
     const potRepository = getRepository(PotEntity);
     const potList = await potRepository.find({
