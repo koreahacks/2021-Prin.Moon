9f376dcd085197d67a8d8d7e37b5da4fda8815c0
박진용 <jinyoung1997@gmail.com>
2021-01-09 12:44:38 +0900

    chore : add config

--- server/src/config/orm.ts
+++ server/src/config/orm.ts
@@ -1,7 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
 export const ormConfig = {
   type: process.env.TYPEORM_CONNECTION || "mysql",
   host: process.env.TYPEORM_HOST || "localhost",
--- server/src/loader/index.ts
+++ server/src/loader/index.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
 import createDBConnection from "./mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default async (app: express.Application) => {
   await createDBConnection();
