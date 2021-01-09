56214b530b1bc88768d854db99165e92f5258252
박진용 <jinyoung1997@gmail.com>
2021-01-10 01:59:35 +0900

    fix : pot entity 수정 및 api 반환 수정
    category 이름도 반환

--- server/src/user-join-pot/user-join-pot.router.ts
+++ server/src/user-join-pot/user-join-pot.router.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import PotService from "../pot/pot.service";
 import UserJoinPotService from "./user-join-pot.service";
 
 const UserJoinPotRouter = express.Router();
@@ -13,15 +14,23 @@ UserJoinPotRouter.post("/apply", async (req: Request, res: Response) => {
   res.status(code).json(json);
 });
 
-UserJoinPotRouter.put("/confirm/:id", async (req: Request, res: Response) => {
-  const userId = req.user?.id;
-  const { id: potId } = req.params;
-  const { code, json } = await UserJoinPotService.updateUserJoinPot(
-    userId,
-    Number(potId)
-  );
-  res.status(code).json(json);
-});
+UserJoinPotRouter.put(
+  "/confirm/:potId",
+  async (req: Request, res: Response) => {
+    const userId = req.user?.id;
+    const { potId } = req.params;
+    const { code, json } = await UserJoinPotService.updateUserJoinPot(
+      userId,
+      Number(potId)
+    );
+
+    if (code === 200) {
+      await PotService.updateJoinedPeople(Number(potId));
+    }
+
+    res.status(code).json(json);
+  }
+);
 
 UserJoinPotRouter.delete("/cancel/:id", async (req: Request, res: Response) => {
   const userId = req.user?.id;
