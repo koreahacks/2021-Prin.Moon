c7ca1e4d6a35674ea14eea7d66a36524f685569d
2oneweek <dlgkswn885@korea.ac.kr>
2021-01-10 05:47:28 +0900

    fix: mypage는 위치 설정 없어도 접근가능

--- client/src/hoc/Auth.js
+++ client/src/hoc/Auth.js
@@ -17,7 +17,13 @@ export default function Auth(Component, loginRequired) {
             history.push("/login");
           }
           if (res.status === 200 && !loginRequired) {
-            if (!myLocation.place && window.location.pathname !== "/") {
+            if (
+              !myLocation.place &&
+              (window.location.pathname !== "/" ||
+                window.location.pathname !== "/mypage" ||
+                window.location.pathname !== "/recruitedParty" ||
+                window.location.pathname !== "/participatedParty")
+            ) {
               if (window.location.pathname === "/address/confirm") {
                 history.replace("/address/confirm");
               } else {
