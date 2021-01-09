43ab1f199caca75c3f5364245efb83cafa778545
moaikang <kk_w1207@naver.com>
2021-01-09 23:36:17 +0900

    [FE] Feat : :zap:️ KakaoMap 컴포넌트 제작
    좌표를 보여주는 KakaoMap 컴포넌트를 제작하였다.

---
+++ client/src/components/kakaoMap.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef } from "react";
+const { kakao, innerHeight } = window;
+
+const mapStyle = {
+  width: "100%",
+  height: "100%",
+  borderRadius: "20px",
+};
+
+function KakaoMap({ lat, lng }) {
+  const mapContainer = useRef();
+  useEffect(() => {
+    kakao &&
+      kakao.maps.load(() => {
+        let el = mapContainer.current;
+        let map = new kakao.maps.Map(el, {
+          center: new kakao.maps.LatLng(lat, lng),
+        });
+
+        let markerPosition = new kakao.maps.LatLng(lat, lng);
+
+        let markers = [];
+
+        // 마커를 생성합니다
+        markers.push(new kakao.maps.Marker({ position: markerPosition }));
+
+        // 마커가 지도 위에 표시되도록 설정합니다
+        markers.map((marker) => marker.setMap(map));
+      });
+  }, [lat, lng, kakao]);
+
+  return (
+    <div style={{ height: innerHeight / 3 }}>
+      <div id="map" style={mapStyle} ref={mapContainer}></div>
+    </div>
+  );
+}
+export default KakaoMap;
