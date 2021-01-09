29875135357cd41d7b54b9707b5a619ab1b10440
moaikang <kk_w1207@naver.com>
2021-01-09 16:58:12 +0900

    [FE] Refactor: :rainbow: Button Component 리팩토링
    버튼 컴포넌트로 받아오는 props를 세세하게 명세해줬고,
    기본적인 width를 100%로 설정하였다.

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,46 +1,60 @@
-import React from 'react';
-import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import styled from "styled-components"
-// primary : 주황색 // secondary : 버튼 
+import React from "react";
+import {
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import styled from "styled-components";
+// primary : 주황색 // secondary : 버튼
 
 const Bold = styled.div`
   font-weight: bold;
-`
+`;
 const theme = createMuiTheme({
-    palette: {
-      primary: {
-        main: '#ffae66',
-      },
-      secondary: {
-        main: '#e2e2e2',
-      },
+  palette: {
+    primary: {
+      main: "#ffae66",
     },
-  });
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      borderRadius: 15,
+    secondary: {
+      main: "#e2e2e2",
     },
-    
   },
-}));
+});
 
-// 회색일때는 color: secondary // 주황색 일때는 color: primary 
-export default function ContainedButtons(props) {
-  const classes = useStyles();
+const useStyles = (width = "100%") =>
+  makeStyles((theme) => ({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+        borderRadius: 15,
+        width,
+      },
+    },
+  }))();
+
+// 회색일때는 color: secondary // 주황색 일때는 color: primary
+export default function ContainedButtons({
+  color,
+  children,
+  onClick,
+  ...props
+}) {
+  const classes = useStyles(props.width);
 
   return (
     <div className={classes.root}>
-        <ThemeProvider theme={theme}>
-            <Button fontWeight="fontWeightBold" variant="contained" color={props.color} href="#contained-buttons" onClick={props.onClick}>
-              <Bold>
-                {props.title}
-              </Bold>
-            </Button>
-        </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Button
+          fontWeight="fontWeightBold"
+          variant="contained"
+          color={color}
+          href="#contained-buttons"
+          onClick={onClick}
+        >
+          <Bold>{children}</Bold>
+        </Button>
+      </ThemeProvider>
     </div>
   );
 }
