4869bba33363deab17488a8b1c582d1b739d7f80
moaikang <kk_w1207@naver.com>
2021-01-09 17:05:51 +0900

    [FE] Feat : :zap:️ 파티생성 카테고리 선택 페이지 생성
     파티생성 카테고리 선택 페이지를 만들었다.

--- client/src/pages/CategorySelectPage.jsx
+++ client/src/pages/CategorySelectPage.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+import Background from "../components/Background";
+import Grid from "../components/Grid";
+import Title from "../components/Title";
+import TransparentHeader from "../components/TransparentHeader";
+import Button from "../components/Button";
+
+const ButtonWrapper = styled.div`
+  margin-top: 4vh;
+`;
 
 export default function CategorySelectPage() {
-  return <div></div>;
+  return (
+    <Background>
+      <TransparentHeader type="Close" />
+      <Grid>
+        <Title>카테고리를 골라주세요</Title>
+        <ButtonWrapper>
+          <Button color="primary" width="100%">
+            배달
+          </Button>
+          <Button color="primary" width="100%">
+            OTT
+          </Button>
+          <Button color="primary" width="100%">
+            기타
+          </Button>
+        </ButtonWrapper>
+      </Grid>
+    </Background>
+  );
 }
