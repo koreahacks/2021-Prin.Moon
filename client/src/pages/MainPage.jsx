2e56697c17fa612cd0c3ac6bd10ecffa2d16a29f
unknown <wnaltnr95@hanyang.ac.kr>
2021-01-09 20:32:49 +0900

    feat: 파티 컴포넌트 완성
    1.파티 컴포넌트 누르면 모달 켜짐

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Counter from "../components/CounterExample";
 import RecoilExample from "../components/RecoilExample";
+import Button from "../components/Button";
+import Modal from "../components/Modal";
 export default function MainPage() {
-  return (
-    <div>
-      <Counter />
-      <RecoilExample />
-    </div>
-  );
+  return <div>
+  <Button title={"Hera"} color={"primary"}></Button>
+  <Modal title={"정문 GS 4캔 만원"} contents={"hihi"} buttons={
+    [<Button title={"Hera"} color={"primary"}></Button>]
+  } openButtonTitle={"열기"} openButtonColor={"primary"}></Modal>
+</div>;
 
 }
